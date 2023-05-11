6f1f402a9181ad399986e16384a11019b27b9136
ndishaw <ndichumuriithi@gmail.com>
2023-05-11 03:01:13 +0000

    Sendgrid & domain configuration

---
+++ .history/hcklServer/server_20230502162954.js
@@ -0,0 +1,47 @@
+import nodemailer from "nodemailer";
+
+const express = require("express");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/api/send-email", (req, res) => {
+  const { email, cartItems } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "SendGrid",
+    auth: {
+      user: process.env.SENDGRID_USERNAME,
+      pass: process.env.SENDGRID_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: "your-email@example.com",
+    to: email,
+    subject: "Shopping Cart Contents",
+    html: `<p>Here are the contents of your shopping cart:</p><ul>${cartItems
+      .map((item) => `<li>${item}</li>`)
+      .join("")}</ul>`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send("Failed to send email");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.send("Email sent successfully");
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
---
+++ .history/src/components/EmailOrderForm_20230416040232.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
+// import { useCart } from "../../cart-context";
+import { useStateValue } from "../context/StateProvider";
+import { actionType } from "../context/reducer";
+import toast, { Toaster } from "react-hot-toast";
+
+const EmailOrderForm = () => {
+//   const { cartItems } = useCart();
+  const [{  cartItems, user }, dispatch] = useStateValue();
+  const [flag, setFlag] = useState(1);
+  const [tot, setTot] = useState(0);
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_0wmgnbl",
+        "template_u07hvsd",
+        form.current,
+        "Z0CbWT44L2zlf5Ag-"
+      )
+      .then(
+        (result) => {
+          console.log("SUCCESS!");
+          console.log(result.text);
+        },
+        (error) => {
+        //   console.log("FAILED!");
+          console.log(error.text);
+          toast.error(error.text);
+        }
+      );
+    console.log(cartItems);
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      {/* <label>Name</label> */}
+      {/* <input type="text" name="user_name" /> */}
+      <label>Email</label>
+      <input type="email" name="email" value={user.email} />
+      <label>Note</label>
+      <textarea name="message" />
+      <ul name="order">
+        {cartItems.map((item) => (
+          <li className="cart-item" key={item.index}>
+            {/* <div
+              className="cart-item-image"
+              style={{
+                backgroundImage: `url(${item.image})`,
+                width: "100px",
+                height: "100px",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+              alt={item.name}
+            ></div> */}
+            <span className="cart-item-name">{item.title}</span>
+            <span className="cart-item-price">{item.price}&nbsp;Kč</span>
+          </li>
+        ))}
+      </ul>
+      <button className="cart-order" type="submit">
+        Order
+      </button>
+    </form>
+  );
+};
+
+export default EmailOrderForm;
